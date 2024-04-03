f6a6972c047019870fdc042e2c506ae0d54ce9ba
ivan <imtambovtcev@gmail.com>
2024-04-03 14:41:30 +0000

    + ureg comment

--- orcaparse/units_and_constants.py
+++ orcaparse/units_and_constants.py
@@ -1,4 +1,5 @@
 from pint import UnitRegistry
 
-# Create a UnitRegistry instance
+
 ureg = UnitRegistry()
+"""Unit registry for pint, use this to define units and constants, do not create a new one"""
